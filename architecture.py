5a7104c5ae523bc0f331a5527c2e35794ca6561d
Martin Oywa <martinoywa2@gmail.com>
2019-06-03 17:18:09 +0300

    add model architecture file

---
+++ architecture.py
@@ -0,0 +1,13 @@
+"""Contains two helper functions.
+    get_model(): Loads the trainded model
+    get_tensor(): Persorms transforms on the input image
+"""
+
+def get_model():
+    """Returns loaded model."""
+    return ''
+
+def get_tensor(image_bytes):
+    """Returns transformed image."""
+    return ''
+
