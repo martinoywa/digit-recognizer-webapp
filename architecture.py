4b59faaa9f7d943b5fc7265b9763c1d5b79ddcd6
Martin Oywa <martinoywa2@gmail.com>
2019-06-03 17:25:06 +0300

    defined the model network and get_model function

--- architecture.py
+++ architecture.py
@@ -1,11 +1,42 @@
-"""Contains two helper functions.
+"""Contains model architecture class and two helper functions.
     get_model(): Loads the trainded model
     get_tensor(): Persorms transforms on the input image
 """
+import io
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import transforms
+from PIL import Image
+
+# architecture
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # linear layer (784 -> 1 hidden node)
+        self.fc1 = nn.Linear(28 * 28, 512)
+        self.fc2 = nn.Linear(512, 512)
+        self.fc3 = nn.Linear(512, 10)
+        self.dropout = nn.Dropout(p=.5)
+
+    def forward(self, x):
+        # flatten image input
+        x = x.view(-1, 28 * 28)
+        # add hidden layer, with relu activation function
+        x = self.dropout(F.relu(self.fc1(x)))
+        x = self.dropout(F.relu(self.fc2(x)))
+        x = F.log_softmax(self.fc3(x), dim=1)
+        
+        return x
 
 def get_model():
     """Returns loaded model."""
-    return ''
+    chechpoint = 'model.pt'
+    model = Net()
+    model.load_state_dict(torch.load(chechpoint, map_location='cpu'), strict=False)
+    model.eval()
+    
+    return model
 
 def get_tensor(image_bytes):
     """Returns transformed image."""
