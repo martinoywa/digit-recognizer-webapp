bef48880529dad47978952f2b82e6598411d3080
Martin Oywa <martinoywa2@gmail.com>
2019-06-03 23:28:43 +0300

    add new model

--- architecture.py
+++ architecture.py
@@ -14,13 +14,12 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         #convolutional layer
-        self.conv1 = nn.Conv2d(1, 16, 3, padding=1)
-        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
-        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
+        self.conv1 = nn.Conv2d(1, 32, 3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
         # max pooling layer
         self.pool = nn.MaxPool2d(2, 2)
         # fully connected layers
-        self.fc1 = nn.Linear(64 * 4 * 4, 512)
+        self.fc1 = nn.Linear(64 * 7 * 7, 512)
         self.fc2 = nn.Linear(512, 64)
         self.fc3 = nn.Linear(64, 10)
         # dropout
@@ -30,9 +29,8 @@ class Net(nn.Module):
         # add sequence of convolutional and max pooling layers
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x)))
         # flatten image input
-        x = x.view(-1, 64 * 4 * 4)
+        x = x.view(-1, 64 * 7 * 7)
         # add hidden layer, with relu activation function
         x = self.dropout(F.relu(self.fc1(x)))
         x = self.dropout(F.relu(self.fc2(x)))
