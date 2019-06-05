b5479a7dd761c886dbbbe424648a78e908f4cac3
Martin Oywa <martinoywa2@gmail.com>
2019-06-05 20:41:58 +0300

    return prediction

--- inference.py
+++ inference.py
@@ -1,8 +1,11 @@
 from .architecture import get_tensor, get_model
+import torch
 
 model = get_model()
 
 def get_image_label(image_bytes):
     tensor = get_tensor(image_bytes)
     output = model.forward(tensor)
-    print(output)
+    _, pred = torch.max(output, 1)
+    
+    return pred.item()
