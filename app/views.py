f1a956ade7373fdfbe4a24730fc85ae8a012c8e4
Martin Oywa <martinoywa2@gmail.com>
2019-09-08 10:19:14 +0300

    PEP8

--- app/views.py
+++ app/views.py
@@ -1,14 +1,15 @@
-from flask import Blueprint, request, render_template, redirect, url_for
+from flask import Blueprint, request, render_template
 from .model_files.inference import get_image_label
 
 # initialize the main blueprint
 main = Blueprint('main', __name__)
 
+
 @main.route('/', methods=['GET', 'POST'])
 def upload_image():
     if request.method == 'GET':
         return render_template('index.html')
-    
+
     if request.method == 'POST':
         print(request.files)
         if 'image_file' not in request.files:
@@ -17,5 +18,5 @@ def upload_image():
         file = request.files['image_file']
         image = file.read()
         label = get_image_label(image_bytes=image)
-        
+
         return render_template('results.html', label=label)
